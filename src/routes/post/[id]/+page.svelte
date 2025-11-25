0f7fa014a3d1bbfad5a1091eebbb545b11f65661
khmerweb <vuthdevelop1@gmail.com>
2025-11-25 15:09:28 +0700

    Make it better

--- src/routes/post/[id]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    let { data } = $props()
-    import Layout from '$lib/components/Layout.svelte'
-    import Post from '$lib/components/Post.svelte';
-</script>
-
-<Layout { data } >
-    <Post { data } />
-</Layout>
