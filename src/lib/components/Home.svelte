557cd0a20084ce501404829f68373c2389b9e663
khmerweb <vuthdevelop1@gmail.com>
2025-11-28 22:17:20 +0700

    Make it better

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -589,7 +589,7 @@
             </div>
             <div class="play-news">
                 <button onclick={()=>nextPrevious('news','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('news')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('news')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('news')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('news','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -611,7 +611,7 @@
             </div>
             <div class="play-movie">
                 <button onclick={()=>nextPrevious('movie','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('movie')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('movie')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('movie')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('movie','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -633,7 +633,7 @@
             </div>
             <div class="play-travel">
                 <button onclick={()=>nextPrevious('travel','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('travel')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('travel')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('travel')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('travel','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -655,7 +655,7 @@
             </div>
             <div class="play-doc">
                 <button onclick={()=>nextPrevious('doc','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('doc')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('doc')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('doc')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('doc','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -677,7 +677,7 @@
             </div>
             <div class="play-web">
                 <button onclick={()=>nextPrevious('web','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('web')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('web')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('web')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('web','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -699,7 +699,7 @@
             </div>
             <div class="play-sport">
                 <button onclick={()=>nextPrevious('sport','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('sport')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('sport')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('sport')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('sport','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -721,7 +721,7 @@
             </div>
             <div class="play-food">
                 <button onclick={()=>nextPrevious('food','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('food')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('food')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('food')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('food','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -743,7 +743,7 @@
             </div>
             <div class="play-music">
                 <button onclick={()=>nextPrevious('music','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('music')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('music')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('music')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('music','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -765,7 +765,7 @@
             </div>
             <div class="play-game">
                 <button onclick={()=>nextPrevious('game','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('game')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('game')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('game')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('game','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
@@ -785,7 +785,7 @@
             </div>
             <div class="play-distraction">
                 <button onclick={()=>nextPrevious('distraction','previous')} title=""><i class="fa fa-step-backward"></i></button>
-                <button onclick={()=>newPlaylist('distraction')} title="new playlist"><i class="fa fa-random"></i></button>
+                <button onclick={()=>newPlaylist('distraction')} title="random playlist"><i class="fa fa-random"></i></button>
                 <button onclick={()=>openPlaylist('distraction')} title="playlist"><i class="fa fa-list" ></i></button>
                 <button onclick={()=>nextPrevious('distraction','next')} title=""><i class="fa fa-step-forward"></i></button>
             </div>
