a41dd159440fad7e9da13d13e446e709f06aa987
khmerweb <vuthdevelop1@gmail.com>
2025-11-30 22:13:03 +0700

    Make it better

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -477,7 +477,7 @@
         grid-column: 2 / span 2;
         grid-row: 2 / span 2;
         position: relative;
-        padding-top: 56%;
+        padding-top: 56.25%;
     }
     .random-video button{
         position: relative;
